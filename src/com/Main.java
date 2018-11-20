950cbb19c3c1f21aabb9d0cf00c5a4d272a2cee4
ivan4000 <sxln1@mail.ru>
2018-11-20 04:32:27 +0300

    JL217runningJavaScript

---
+++ src/com/Main.java
@@ -0,0 +1,67 @@
+package com;
+
+import javax.management.ObjectName;
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class Main {
+    public static void main(String[] args) throws ScriptException, NoSuchMethodException {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("JavaScript");
+        engine.put("k", 4);
+        String code = "print('Hello ');"+
+                "function f(a){return a+6;};"+
+                "var n =1+2;" +
+                "n+3+k";
+        Object o = engine.eval(code);
+        System.out.println(o);
+         Object o2=((Invocable )engine).invokeFunction("f",1);
+        System.out.println(o2);
+        Object o3=((Invocable )engine).invokeFunction("f","1");
+        System.out.println(o3);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
